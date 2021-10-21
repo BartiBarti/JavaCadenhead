72ffaf2def14b22c46e9627c2dc422ee44a46227
Bart <bakosberg@gmail.com>
2021-10-22 01:59:00 +0200

    add to github last exercise from hour 5

---
+++ Java24/src/main/java/com/java24hours/Kwadrat1.java
@@ -0,0 +1,12 @@
+package com.java24hours;
+
+class Kwadrat1 {
+    public static void main(String[] arguments) {
+       System.out.print("Suma kwadratu moich liczb to ");
+       int x = 5;
+       int y = 3;
+       int answer = x * x + y * y;
+       System.out.println(answer);
+    }
+}
+
